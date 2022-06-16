453df83ae90b79e3b20e252f8dcaf4407987fb19
mohidenabdulkarim <mohidendj@gmail.com>
2022-06-16 03:31:57 +0300

    added dynamic revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,13 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  //check for secret
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    await res.unstable_revalidate('/');
+    return res.json({ revalidate: true });
+  } catch (e) {
+    return res.status(500).send("Error revalidating");
+  }
 }
